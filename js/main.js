41fd24b8183f3c03e7ce23b5aa574b45b1248f17
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-03-08 16:29:46 -0300

    Finalizado

--- js/main.js
+++ js/main.js
@@ -32,7 +32,8 @@ const updateClient = (index, client) => {
     const dbClient = readClient()
     dbClient[index] = client
     setLocalStorage(dbClient)
-}//CRUD - Update
+}
+//CRUD - Update
 
 //CRUD - Delete
 const deleteClient = (index) =>{
@@ -61,18 +62,26 @@ const saveClient = () => {
         const client = {
             nome: document.getElementById('nome').value,
             email: document.getElementById('email').value,
-            celular: document.getElementById('cidade').value,
+            celular: document.getElementById('celular').value,
             cidade: document.getElementById('cidade').value
         }
+        const index = document.getElementById('nome').dataset.index
+        if(index == 'new'){
         createClient(client)
         updateTable()
         closeModal()
+        }else{
+            updateClient(index, client);
+            updateTable()
+            closeModal()
+            
+        }
     }
 }
 //interação com o layout
 
 
-const createRow = (client) => {
+const createRow = (client, index) => {
     const newRow = document.createElement('tr')
     newRow.innerHTML = `
     <td>${client.nome}</td>
@@ -80,35 +89,79 @@ const createRow = (client) => {
     <td>${client.celular}</td>
     <td>${client.cidade}</td>
     <td>
-        <button type="button" class="button green">editar</button>
-        <button type="button" class="button red">excluir</button>
-    </td>
+            <button type="button" class="button green" id="edit-${index}">Editar</button>
+            <button type="button" class="button red" id="delete-${index}" >Excluir</button>
+        </td>
     `
     document.querySelector('#tbClient>tbody').appendChild(newRow)
 }
 
 const clearTable = () =>{
-    const rows = document.querySelectorAll('#tableClient>tbody tr')
+    const rows = document.querySelectorAll('#tbClient>tbody tr')
     rows.forEach(row => row.parentNode.removeChild(row))
 }
 
 
 const updateTable = () => {
     const dbClient = readClient()
+    clearTable()
     dbClient.forEach(createRow)
+}
+
+const fillFilds = (client) =>{
+    document.getElementById('nome').value = client.nome
+    document.getElementById('email').value = client.email
+    document.getElementById('celular').value = client.celular
+    document.getElementById('cidade').value = client.cidade
+    document.getElementById('nome').dataset.index = client.index
+    }
+
+const editClient = (index)=> {
+     const client = readClient()[index]
+     client.index = index
+        fillFilds(client)
+        openModal()
+    }
+
+
+
+
+const editDelete = (e) => {
+    if(e.target.type == 'button'){
+        
+        const [action, index] = e.target.id.split('-')
+
+        if(action == "edit"){
+            editClient(index)
+        }else {
+            const client =readClient()[index]
+            const response = confirm(`
+            Deseja Realmente excluir o cliente ${client.nome}
+            `)
+            if(response){
+            deleteClient(index)
+            updateTable()
+        }
+
+        }
+
+
+    }
 }
 
 updateTable()
 
 //Eventos
 document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
+        .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
+        .addEventListener('click', closeModal)
 
-    document.getElementById('salvar')
-            .addEventListener('click',saveClient)
+document.getElementById('salvar')
+        .addEventListener('click',saveClient)
+document.querySelector('#tbClient>tbody')
+        .addEventListener('click', editDelete)
 //eventos
 
 
